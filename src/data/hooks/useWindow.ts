a5931a16f02a20a52844b86d3327970068cc97d4
Felipe <felipesullivan50@gmail.com>
2023-02-24 09:22:25 -0300

    🐳 chore: Dep and architecture
    All Notion FrontEnd dependencies list includes and a architecture sugestion

---
+++ src/data/hooks/useWindow.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+export const useWindow = () => {
+  const [width, setWidth] = useState(
+    typeof window !== 'undefined' ? window.innerWidth : 0
+  );
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  return {
+    width,
+  };
+};
