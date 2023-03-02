192e5b62b40984f7c49774b0f3327eee7785b903
Felipe <felipesullivan50@gmail.com>
2023-03-02 14:55:23 -0300

    ✨ feat: Google and GitHub
    new auth methods

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,15 +27,17 @@ export const getServerSideProps: GetServerSideProps = async ({req}: GetServerSid
 };
 
 export default function Home({data}: HomeProps) {
-  const normalizedData: Data = JSON.parse(data);
-  console.log(normalizedData);
+  const session: Data = JSON.parse(data);
+  console.log(session);
 
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <h1>DevStream</h1>
-      {normalizedData && <Image src={normalizedData.user.image} alt={normalizedData.user.name} height={100} width={100}/>}
+      {session && <Image src={session.user.image} alt={session.user.name} height={100} width={100}/>}
       <div className='flex gap-4'>
-        <button onClick={() => signIn('linkedin')}>Entrar</button>
+        <button onClick={() => signIn('linkedin')}>Entrar com Linkedin</button>
+        <button onClick={() => signIn('google')}>Entrar com Google</button>
+        <button onClick={() => signIn('github')}>Entrar com Github</button>
         <button onClick={() => signOut()}>Sair</button>
       </div>
     </div>
